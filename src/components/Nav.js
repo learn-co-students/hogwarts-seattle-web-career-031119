47e5e775bfa41fb1250b54e0dbaebbab268832cf
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-05-17 09:53:44 -0700

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,27 @@
 import piggy from '../porco.png'
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
 			<div className="TwirlyPig">
-				<a href="https://www.lowes.com/pd/LG-24-7-cu-ft-French-Door-Refrigerator-with-Ice-Maker-Stainless-steel/4746231">
+				<a href="Hello..._my_name_is_computer_who_are_you??">
 					<img src={piggy} className="App-logo" alt="piggy" />
 				</a>
 			</div>
 			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<div>
+			<select onChange={props.handleSelect}>
+			  <option default value="volvo">Sorty by</option>
+			  <option value="weight">weight</option>
+			  <option value="name">name</option>
+			</select>{" "}
+			<select onChange={props.handleFilter}>
+			  <option default value="peugeot">Filter by</option>
+			  <option value="grease">grease</option>
+				</select>
+			</div>
 		</div>
 	)
 }
